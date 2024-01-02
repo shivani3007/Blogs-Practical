b092ca51392876302ae0d3eea598609b0b105837
shivani3007 <shivanichampaneria27@gmail.com>
2024-01-02 07:51:29 -0800

    Changes regarding userSchema and utils

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -21,7 +21,6 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true,
-        select:false,
     },
     role : {
         type: mongoose.Schema.Types.ObjectId,
--- utils/helper.js
+++ utils/helper.js
@@ -2,12 +2,13 @@ const bcrypt = require('bcrypt');
 
 exports.verifyPassword = async (plainPassword, hashedPassword) => {
     try{
+        console.log(plainPassword,hashedPassword);
         const isPasswordMatched = await bcrypt.compare(plainPassword, hashedPassword);
         if(isPasswordMatched){
             return true;
         }
         return false;
     }catch(error){
-        log(error,'error')
+        console.log(error,'error')
     }
 }
